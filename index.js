5a60649b430c8e603fe444fa7bd19fd01d362752
Yen Chen Chen <yenchen@YendeMBP.lan>
2024-07-06 18:08:37 +1000

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('test!!!')
+  res.send('Hello world')
 })
 
 app.listen(port, () => {
