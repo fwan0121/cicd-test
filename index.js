71416b384600f289bec12886a38bb219880f2626
Yen Chen Chen <yenchen@YendeMBP.lan>
2024-07-06 19:28:47 +1000

    change folder

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const path = require('path')
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
